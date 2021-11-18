e8eb680fa6f91a57ea8d4270c551f8a4a67f17ee
Dorian Gautier <60466418+Seikanuma@users.noreply.github.com>
2021-11-18 11:50:41 +0100

    Update and rename CommentairesRepository.php to PostsRepository.php

--- apisymfo/src/Repository/PostsRepository.php
+++ apisymfo/src/Repository/PostsRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\Commentaires;
+use App\Entity\Posts;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
